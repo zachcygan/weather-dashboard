17b64e84aad373c3f15de3df3efd25cfc92f23f1
Zachary Cygan <zachcygan@gmail.com>
2023-02-02 14:53:52 -0800

    added the current city and date as well as temp, wind speed, and humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
 var $todaysForcast = $('#todaysForcast');
 var $citySearch = $('#cityInput');
-var $searchButton = $('#citySearchButton')
+var $searchButton = $('#citySearchButton');
+var $recentSearches = $('#recentSearches');
 var selectedCity;
 
 function saveCityName(event) {
     event.preventDefault();
 
-    selectedCity = $citySearch.val();
+    selectedCity = $citySearch.val().trim();
+
+    var listItem = document.createElement('li').textContent = selectedCity;
+    $recentSearches.removeClass('d-none')
+    $recentSearches.append(listItem);
+
 
     fetchData();
+    // setInterval(fetchData, 1000);
 }
 
 function fetchData() {
@@ -28,10 +35,30 @@ function fetchData() {
         console.log(locationLon)
 
         
-        fetch('api.openweathermap.org/data/2.5/forecast?lat=' + locationLat + '&lon=' + locationLon + '&appid=9d7ee1a0c89726386b718cd593b3c6c3')
+        fetch('http://api.openweathermap.org/data/2.5/weather?lat=' + locationLat + '&lon=' + locationLon + '&appid=9d7ee1a0c89726386b718cd593b3c6c3&units=imperial')
             .then((response) => response.json())
             .then((data) => {
                 console.log(data)
+
+                var cityName = data.name;
+                var temp = data.main.temp + '°F';
+                var windSpeed = data.wind.speed + 'mph';
+                var humidity = data.main.humidity + '%';
+
+                var currentCity = document.createElement('h1');
+                var currentTemp = document.createElement('p');
+                var currentWind = document.createElement('p');
+                var currentHumidity = document.createElement('p');
+
+                currentCity.textContent = cityName + ' (' + dayjs().format('MM/DD/YYYY') + ')';
+                currentTemp.textContent = 'Temp: ' + temp;
+                currentWind.textContent = 'Wind: ' + windSpeed;
+                currentHumidity.textContent = 'Humidity: ' + humidity;
+
+                $todaysForcast.append(currentCity);
+                $todaysForcast.append(currentTemp);
+                $todaysForcast.append(currentWind);
+                $todaysForcast.append(currentHumidity);
             });
     });
     
